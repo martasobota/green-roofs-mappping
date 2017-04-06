2f4d8706f36741ecd33eb93f7375c13621727b04
Marta Żaryn <martazaryn@gmail.com>
2017-04-06 15:51:31 +0200

    Before adding google maps settings

--- Green_Roof_MapPy/Green_Roof_MapPy/settings.py
+++ Green_Roof_MapPy/Green_Roof_MapPy/settings.py
@@ -137,4 +137,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 STATIC_URL = '/static/'
