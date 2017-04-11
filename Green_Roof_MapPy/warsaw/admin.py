4f4e3e2bbbe0815bcfe44307150a6a0348e26f95
martazaryn <martazaryn@gmail.com>
2017-04-11 17:59:18 +0200

    #

--- Green_Roof_MapPy/warsaw/admin.py
+++ Green_Roof_MapPy/warsaw/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+# from django.contrib import admin
+# from django.db import models
+# from django_google_maps import widgets as map_widgets
+# from django_google_maps import fields as map_fields
+# from warsaw.models import City, District, GreenRoof, Rental
+# from mapwidgets.widgets import GooglePointFieldWidget
+# from django.contrib.gis.db import models
 
-# Register your models here.
+# class RentalAdmin(admin.ModelAdmin):
+#     formfield_overrides = {
+#         map_fields.AddressField: {'widget': map_widgets.GoogleMapsAddressWidget},
+#     }
+
+
+# class CityAdmin(admin.ModelAdmin):
+#     formfield_overrides = {
+#         models.PointField: {"widget": GooglePointFieldWidget}
+#     }
