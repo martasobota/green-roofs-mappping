34ff81ccfdb0637c98050da40bbff40dd1b727f6
martazaryn <martazaryn@gmail.com>
2017-04-21 12:21:22 +0200

    Cleaned load.py

---
+++ Green_Roof_MapPy/warsaw/load.py
@@ -0,0 +1,16 @@
+import os
+# from django.contrib.gis.utils import LayerMapping
+# from .models import GreenRoof
+
+# green_roofs = {
+#     'district' : 'DISTRICT',
+#     'roof_address' : 'ADDRESS',
+#     'roof_type' : 'TYPE',
+#     'area' : 'AREA',
+#     'total_place_area' : 'TOTAL_AREA',
+#     'access' : 'ACCESS',
+#     'ownership_type' : 'OWNERSHIP',
+#     'additional_info' : 'INFO',
+#     'point' : 'POINT',
+    
+# }
